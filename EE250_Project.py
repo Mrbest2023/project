4ffefef568794cd88df77ee5b89924ab2f1664b2
Mrbest2023 <btbest@usc.edu>
2023-05-02 21:41:54 -0700

    d

--- EE250_Project.py
+++ EE250_Project.py
@@ -1,25 +1,26 @@
 import grovepi
 from grovepi import *
 from grove_rgb_lcd import *
-from gpiozero import Button
-from signal import pause
+import time
 
-button = Button(2)
+button = 3
+flag=1
 
 sound_sensor=0
 grovepi.pinMode(sound_sensor, "INPUT")
+grovepi.pinMode(button,"INPUT")
 
 led=5
 sensor_value=[]
 
 if __name__ == '__main__':
-
     while True:
-        button.when_pressed = sound_sensor.on
-        while True:
+        if grovepi.digitalRead(button) == 1:
             sensor_value.appened(grovepi.analogRead(sound_sensor))
-            if button.when_released:
-                sound_sensor.off
-                break   
-        
-        pause()
+            flag=0
+        else:
+            if flag==0:
+                #Analyze
+                flag=1
+
+        #print to led
