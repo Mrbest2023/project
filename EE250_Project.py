648ce721a5ba08df2ec1f687c9883aabd55518b0
Mrbest2023 <btbest@usc.edu>
2023-05-02 17:08:37 -0700

    h

--- EE250_Project.py
+++ EE250_Project.py
@@ -1,6 +1,9 @@
 import grovepi
 from grovepi import *
 from grove_rgb_lcd import *
+from gpiozero import Button
+
+buton = Button(2)
 
 sound_sensor=0
 grovepi.pinMode(sound_sensor, "INPUT")
@@ -9,20 +12,17 @@ led=5
 
 
 
+if __name__ == '__main__':
 
+    if     : 
+        while True:
+            try:
+                sensor_value=grovepi.analogRead(sound_sensor)
+                setText_norefresh()
 
+            except KeyboardInterrupt as e:
+                print(str(e))
+                setText("")
+                break
 
-while True:
-    try:
-        sr=32
-        sensor_value=grovepi.analogRead(sound_sensor)
-        frequency,X=frequency_spectrum(sensor_value, sr)
-        type=note(frequency)
-        setText_norefresh(type)
-
-    except KeyboardInterrupt as e:
-        print(str(e))
-        setText("")
-        break
-
-    sleep(0.05)
+            sleep(0.05)
