fb9857981ee65feb9550621dab08277f26737f64
Mrbest2023 <btbest@usc.edu>
2023-05-02 17:11:20 -0700

    d

--- EE250_Project.py
+++ EE250_Project.py
@@ -2,27 +2,24 @@ import grovepi
 from grovepi import *
 from grove_rgb_lcd import *
 from gpiozero import Button
+from signal import pause
 
-buton = Button(2)
+button = Button(2)
 
 sound_sensor=0
 grovepi.pinMode(sound_sensor, "INPUT")
 
 led=5
-
-
+sensor_value=[]
 
 if __name__ == '__main__':
 
-    if     : 
+    while True:
+        button.when_pressed = sound_sensor.on
         while True:
-            try:
-                sensor_value=grovepi.analogRead(sound_sensor)
-                setText_norefresh()
-
-            except KeyboardInterrupt as e:
-                print(str(e))
-                setText("")
-                break
-
-            sleep(0.05)
+            sensor_value.appened(grovepi.analogRead(sound_sensor))
+            if button.when_released:
+                sound_sensor.off
+                break   
+        
+        pause()
