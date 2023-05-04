5f77199bc07bed6c694bd285fe21cf9fc9358a53
Mrbest2023 <btbest@usc.edu>
2023-05-04 15:34:31 -0700

    u

--- EE250_Project.py
+++ EE250_Project.py
@@ -4,54 +4,7 @@ from grove_rgb_lcd import *
 import time
 import paho.mqtt.client as mqtt
 import socket
-from scipy import fft, arange
-import numpy as np
-import os
-import time 
 
-
-
-def frequency_spectrum(x, sf):
-    """
-    Derive frequency spectrum of a signal from time domain
-    :param x: signal in the time domain
-    :param sf: sampling frequency
-    :returns frequencies and their content distribution
-    """
-    x = x - np.average(x)  # zero-centering
-
-    n = len(x)
-    k = arange(n)
-    tarr = n / float(sf)
-    frqarr = k / float(tarr)  # two sides frequency range
-
-    frqarr = frqarr[range(n // 2)]  # one side frequency range
-
-    x = fft(x) / n  # fft computing and normalization
-    x = x[range(n // 2)]
-
-    return frqarr, abs(x)
-
-
-def note(freq):
-    if freq > 80 and freq < 86:
-        return("E")
-    if freq > 87 and freq < 93:
-        return("F")
-    if freq > 94 and freq < 103:
-        return("G")
-    if freq > 104 and freq < 115:
-        return("A")
-    if freq > 116 and freq < 124:
-        return("B")
-    if freq > 125 and freq < 138:
-        return("C")
-    if freq > 139 and freq < 155:
-        return("D")
-    if freq > 156 and freq < 174:
-        return("E")
-    else:
-        return("OUT OF RANGE")
 
 def on_connect(client, userdata, flags, rc): 
     print("Connected to server (i.e., broker) with result code "+str(rc))
@@ -67,23 +20,26 @@ led=5
 sensor_value=[]
 
 if __name__ == '__main__': 
-    ip_address='68.181.32.115' 
-    client = mqtt.Client()
-    client.loop_start()
-    time.sleep(1)
-
+  
     while True:
+        #subscribe
+        client = mqtt.Client()
+        client.on_message = on_message
+        client.on_connect = on_connect
+        client.connect(host="68.181.32.115", port=11000, keepalive=60)
+    
+
+        #publish
+        ip_address='68.181.32.115' 
+        client = mqtt.Client()
+        time.sleep(1)
         
         if grovepi.digitalRead(button) == 1:
             sensor_value.appened(grovepi.analogRead(sound_sensor))
             flag=0
         else:
             if flag==0:
-                sr=32
-                frequency,X=frequency_spectrum(sensor_value, sr)
-                type=note(frequency)
-                client.publish(type)
-                
+                client.publish(sensor_value)
                 flag=1
 
         #print to led
--- server.py
+++ server.py
@@ -1,17 +1,18 @@
 import paho.mqtt.client as mqtt 
+from notes import frequency_spectrum, note
 
 
 def on_connect(client, userdata, flags, rc):
     print("Connected to server (i.e., broker) with result code "+str(rc))
-    client.subscribe()
+    client.subscribe("btbest/sensor_value")
 
-    client.message_callback_add("btbest/note", on_message_from_ipinfo)
+    client.message_callback_add("btbest/sensor_value", on_message_from_ipinfo)
 
 def on_message(client, userdata, msg): 
     print("Default callback - topic: " + msg.topic + " msg: " + str(msg.payload, "utf-8"))
 
 def on_message_from_ipinfo(client, userdata, message):
-    print("Custom callback - Note: "+message.payload.decode()) 
+    print("Custom callback - Sensor_value: "+message.payload.decode()) 
 if __name__ == '__main__':
 
     client = mqtt.Client()
