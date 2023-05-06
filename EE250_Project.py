3c78c1f4ff1d33163488f68acd99ba05d1a7569a
Marina <msbraga@usc.edu>
2023-05-05 20:45:51 -0700

    changing everything

--- EE250_Project.py
+++ EE250_Project.py
@@ -5,28 +5,7 @@ import time
 import paho.mqtt.client as mqtt
 import socket
 import json
-import Adafruit_GPIO.SPI as SPI
-import Adafruit_MCP3008
-import RPi.GPIO as GPIO
-try:
-    import RPi.GPIO as GPIO
-except RuntimeError:
-    print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
 
-
-GPIO.setmode(GPIO.BOARD)
-GPIO.setwarnings(False)
-GPIO.setup(11, GPIO.OUT)
-
-print('Reading MCP3008 values, press Ctrl-C to quit...')
-# Print nice channel column headers.
-print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*range(8)))
-print('-' * 57)
-
-
-SPI_PORT   = 0
-SPI_DEVICE = 0
-mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
 def on_connect(client, userdata, flags, rc): 
     print("Connected to server "+str(rc)) 
@@ -65,8 +44,7 @@ if __name__ == '__main__':
         client.connect(host="broker.hivemq.com", port=1883, keepalive=60)
         
         if grovepi.digitalRead(button) == 1:
-            s = mcp.read_adc(i)
-            #s = grovepi.analogRead(sound_sensor)
+            #s = grovepi.analogRead(sound_sensor) #Would be used with a microphone
             if s>0:
                 sensor_data.append(s)
                 flag=0
